b46d7792130f7e5fefc4a19770a5230a995b03ce
Robin Kukke <robkukke@users.noreply.github.com>
2021-09-27 16:46:45 +0300

    Kodune_ylesanne_4

--- add_films.php
+++ add_films.php
@@ -3,25 +3,55 @@ require_once "../../config.php";
 require_once "fnc_film.php";
 // echo $server_host;
 $author_name = "Robin Kukke";
-$film_store_notice = null;
+$title_input_empty = null;
+$year_input_empty = null;
+$duration_input_empty = null;
+$genre_input_empty = null;
+$studio_input_empty = null;
+$director_input_empty = null;
 // kas klikiti submit nuppu
 if (isset($_POST["film_submit"])) {
+    $title_input = test_input($_POST["title_input"]);
+    $year_input = filter_var(test_input($_POST["year_input"]), FILTER_VALIDATE_INT);
+    $duration_input = filter_var(test_input($_POST["duration_input"]), FILTER_VALIDATE_INT);
+    $genre_input = test_input($_POST["genre_input"]);
+    $studio_input = test_input($_POST["studio_input"]);
+    $director_input = test_input($_POST["director_input"]);
     if (
-        !empty($_POST["title_input"]) and
-        !empty($_POST["genre_input"]) and
-        !empty($_POST["studio_input"]) and
-        !empty($_POST["director_input"])
+        !empty($title_input) and
+        !empty($year_input) and
+        !empty($duration_input) and
+        !empty($genre_input) and
+        !empty($studio_input) and
+        !empty($director_input)
     ) {
         $film_store_notice = store_film(
-            $_POST["title_input"],
-            $_POST["year_input"],
-            $_POST["duration_input"],
-            $_POST["genre_input"],
-            $_POST["studio_input"],
-            $_POST["director_input"]
+            $title_input,
+            $year_input,
+            $duration_input,
+            $genre_input,
+            $studio_input,
+            $director_input
         );
     } else {
-        $film_store_notice = "Osa andmeid on puuudu!";
+        if (empty($title_input)) {
+            $title_input_empty = "Sisesta filmi pealkiri!";
+        }
+        if (empty($year_input)) {
+            $year_input_empty = "Sisesta valmimisaasta!";
+        }
+        if (empty($duration_input)) {
+            $duration_input_empty = "Sisesta kestus minutites!";
+        }
+        if (empty($genre_input)) {
+            $genre_input_empty = "Sisesta filmi žanr!";
+        }
+        if (empty($studio_input)) {
+            $studio_input_empty = "Sisesta filmi tootja!";
+        }
+        if (empty($director_input)) {
+            $director_input_empty = "Sisesta filmi lavastaja!";
+        }
     }
 }
 ?>
@@ -38,27 +68,32 @@ if (isset($_POST["film_submit"])) {
 	<p>Õppetöö toimus 2021 sügisel.</p>
 	<hr>
 	<h2>Eesti filmid</h2>
-	<form method="post">
+	<form method="post" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
 		<label for="title_input">Filmi pealkiri: </label>
-		<input id="title_input" name="title_input" placeholder="pealkiri" type="text">
+		<input id="title_input" name="title_input" placeholder="pealkiri" type="text" value="<?= $title_input ?? "" ?>">
+        <?= $title_input_empty ?>
 		<br>
 		<label for="year_input">Valmimisaasta: </label>
-		<input id="year_input" min="1912" name="year_input" type="number" value="<?= date("Y") ?>">
+		<input id="year_input" min="1912" name="year_input" type="number" value="<?= $year_input ?? date("Y") ?>">
+        <?= $year_input_empty ?>
 		<br>
 		<label for="duration_input">Kestus minutites: </label>
-		<input id="duration_input" min="1" name="duration_input" type="number" value="60">
+		<input id="duration_input" min="1" name="duration_input" type="number" value="<?= $duration_input ?? "60" ?>">
+        <?= $duration_input_empty ?>
 		<br>
 		<label for="genre_input">Filmi žanr: </label>
-		<input id="genre_input" name="genre_input" placeholder="žanr" type="text">
+		<input id="genre_input" name="genre_input" placeholder="žanr" type="text" value="<?= $genre_input ?? "" ?>">
+        <?= $genre_input_empty ?>
 		<br>
 		<label for="studio_input">Filmi tootja: </label>
-		<input id="studio_input" name="studio_input" placeholder="tootja" type="text">
+		<input id="studio_input" name="studio_input" placeholder="tootja" type="text" value="<?= $studio_input ?? "" ?>">
+        <?= $studio_input_empty ?>
 		<br>
 		<label for="director_input">Filmi lavastaja: </label>
-		<input id="director_input" name="director_input" placeholder="lavastaja" type="text">
+		<input id="director_input" name="director_input" placeholder="lavastaja" type="text" value="<?= $director_input ?? "" ?>">
+        <?= $director_input_empty ?>
 		<br>
 		<input name="film_submit" type="submit" value="Salvesta">
 	</form>
-	<p><?= $film_store_notice ?></p>
 </body>
 </html>
