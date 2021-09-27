7d9b81cf4aede55b657f128d6bc5d30f003a9303
Robin Kukke <robkukke@users.noreply.github.com>
2021-09-27 16:46:45 +0300

    Kodune_ylesanne_4

--- fnc_film.php
+++ fnc_film.php
@@ -99,3 +99,9 @@ function store_film(
     $conn->close();
     return $success;
 }
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    return htmlspecialchars($data);
+}
